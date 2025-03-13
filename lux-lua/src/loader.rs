00ebf987fdb3662fa8bf223fcf6c2baafa6e4a98
vhyrro <vhyrro@gmail.com>
2025-03-13 16:39:44 +0100

    perf(loader): don't perform expensive filesystem operations
    remove logic for finding rocks from the current project: `lx path` will
    already return the correct paths to the correct versions of rocks.

--- lux-lua/src/loader.rs
+++ lux-lua/src/loader.rs
@@ -1,10 +1,6 @@
 use std::path::PathBuf;
 
-use lux_lib::{
-    lockfile::{LocalPackageId, LocalPackageLockType, Lockfile},
-    package::PackageReq,
-    project::Project,
-};
+use lux_lib::lockfile::{LocalPackageId, Lockfile};
 use mlua::prelude::*;
 use path_absolutize::Absolutize;
 
@@ -84,107 +80,70 @@ pub fn load_loader(lua: &Lua) -> mlua::Result<()> {
 }
 
 pub fn loader(lua: &Lua, module: String) -> mlua::Result<Option<mlua::Function>> {
-    let module_root = match module.split('.').next() {
-        Some(module) => module,
-        None => return Ok(None),
-    };
     let current_file = match current_file(lua).as_str() {
         "stdin" => return Ok(None),
         current_file => PathBuf::from(current_file),
     };
     let current_file = current_file.absolutize().into_lua_err()?;
 
-    match lets_find_up::find_up_with(
-        "lux.lock",
-        lets_find_up::FindUpOptions {
-            cwd: current_file.parent().unwrap(),
-            kind: lets_find_up::FindUpKind::File,
-        },
-    )
-    .into_lua_err()?
+    // Check if we're in a tree by searching the path for a `.lux` directory
+    if let Some(lock_path) = current_file
+        .ancestors()
+        .find(|path| path.file_name().is_some_and(|filename| filename == ".lux"))
     {
-        Some(lock_path) => {
-            // Check if we're in a Project and if so, treat the immediate dependencies as our
-            // targets.
-            match Project::from(lock_path.parent().unwrap()).into_lua_err()? {
-                Some(project) => {
-                    match project
-                        .lockfile()
-                        .into_lua_err()?
-                        .local_pkg_lock(&LocalPackageLockType::Regular)
-                        .has_entrypoint(
-                            &PackageReq::new(module_root.to_string(), None).into_lua_err()?,
-                        ) {
-                        Some(package) => {
-                            let path = project.root().join(format!(
-                                ".lux/{version}/{id}-{package_name}-{package_version}/",
-                                version = lua_version(),
-                                id = package.id(),
-                                package_name = package.name(),
-                                package_version = package.version()
-                            ));
+        // Get the name of the current module, so we can look up its dependencies in the lockfile.
+        let lockfile = Lockfile::new(lock_path.join("lux.lock")).into_lua_err()?;
 
-                            Ok(Some(load_file(lua, module, path)?))
-                        }
-                        None => Ok(None),
-                    }
-                }
-                None => {
-                    // Get the name of the current module, so we can look up its dependencies in the lockfile.
-                    let lockfile = Lockfile::new(lock_path.clone()).into_lua_err()?;
+        // If we're in a lux tree, the path looks like `.lux/5.4/12345678-package_name-1.0.0/src/code.lua`
+        // we need to extract the hash from the path.
 
-                    // If we're in a lux tree, the path looks like `.lux/5.4/12345678-package_name-1.0.0/src/code.lua`
-                    // we need to extract the hash from the path.
+        let module_hash = current_file.iter().rev().find_map(|path| {
+            if let [hash, _name, _version, _rest @ ""] =
+                path.to_str().unwrap().splitn(3, '-').collect::<Vec<&str>>()[..4]
+            {
+                Some(hash)
+            } else {
+                None
+            }
+        });
 
-                    let module_hash = current_file.iter().rev().find_map(|path| {
-                        if let [hash, _name, _version, _rest @ ""] =
-                            path.to_str().unwrap().splitn(3, '-').collect::<Vec<&str>>()[..4]
-                        {
-                            Some(hash)
+        if let Some(module_hash) = module_hash {
+            // NOTE(vhyrro): On safety - it's possible that the user *could* tamper
+            // with the lux tree and malform the package hash. In this case, this
+            // should never cause any security-related problems anyway, as we'll
+            // crash right after this function returns None.
+            if let Some(package) =
+                lockfile.get(unsafe { &LocalPackageId::from_unchecked(module_hash.to_string()) })
+            {
+                return package
+                    .dependencies()
+                    .iter()
+                    .find_map(|dep| {
+                        let dep = lockfile.get(dep).unwrap();
+
+                        if dep.name().to_string() == module {
+                            Some(dep)
                         } else {
                             None
                         }
-                    });
+                    })
+                    .map(|dep| {
+                        let path = lock_path.parent().unwrap().join(format!(
+                            ".lux/{version}/{id}-{package_name}-{package_version}/",
+                            version = lua_version(),
+                            id = dep.id(),
+                            package_name = dep.name(),
+                            package_version = dep.version()
+                        ));
 
-                    if let Some(module_hash) = module_hash {
-                        // NOTE(vhyrro): On safety - it's possible that the user *could* tamper
-                        // with the lux tree and malform the package hash. In this case, this
-                        // should never cause any security-related problems anyway, as we'll
-                        // crash right after this function returns None.
-                        if let Some(package) = lockfile.get(unsafe {
-                            &LocalPackageId::from_unchecked(module_hash.to_string())
-                        }) {
-                            return package
-                                .dependencies()
-                                .iter()
-                                .find_map(|dep| {
-                                    let dep = lockfile.get(dep).unwrap();
-
-                                    if dep.name().to_string() == module {
-                                        Some(dep)
-                                    } else {
-                                        None
-                                    }
-                                })
-                                .map(|dep| {
-                                    let path = lock_path.parent().unwrap().join(format!(
-                                        ".lux/{version}/{id}-{package_name}-{package_version}/",
-                                        version = lua_version(),
-                                        id = dep.id(),
-                                        package_name = dep.name(),
-                                        package_version = dep.version()
-                                    ));
-
-                                    load_file(lua, module, path)
-                                })
-                                .transpose();
-                        }
-                    }
-
-                    Ok(None)
-                }
+                        load_file(lua, module, path)
+                    })
+                    .transpose();
             }
         }
-        None => Ok(None),
+
+        Ok(None)
+    } else {
+        Ok(None)
     }
 }
