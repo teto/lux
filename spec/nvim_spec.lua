31197e4eacecf3df6c21604e007e318c890033b1
Marc Jakobi <marc.jakobi@tiko.energy>
2025-06-15 00:49:22 +0200

    chore: remove some files that should be ignored

--- spec/nvim_spec.lua
+++
@@ -1,6 +0,0 @@
-describe('Test example', function()
-  it('Test can access vim namespace', function()
-    assert(vim, 'Cannot access vim namespace')
-    assert.are.same(vim.trim('  a '), 'a')
-  end)
-end)
